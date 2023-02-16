22099964feafd9c2b43465112271ae4f5b1d9ddc
radness <yhs5128@gmail.com>
2023-02-16 23:42:29 +0900

    init setting

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+function Navbar() {
+  return (
+    <>
+      <div className='navbar'>
+        <div className='navbar-container'>
+
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
