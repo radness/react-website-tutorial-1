807c9ee43fb8059526fe365913e049737da61940
radness <yhs5128@gmail.com>
2023-02-17 00:24:00 +0900

    navbar 소스 파일 생성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        {/* react-router-dom version 6 이상은 Switch 지원을 하지 않는다.
+            Switch -> Routes */}
+        <Routes>
+          <Route path='/' exact />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
